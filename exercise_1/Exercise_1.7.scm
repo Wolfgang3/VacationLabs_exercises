8460486bfa13775109c5f14a27b33ef536ed793b
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-04 16:21:29 +0530

    first day

---
+++ exercise_1/Exercise_1.7.scm
@@ -0,0 +1,3 @@
+Yes.The good-enough test will not be very effective for finding the square roots of very small numbers.
+
+this algorithm will fail because the accuracy of the solution may not be within 0.001 as in the function.
