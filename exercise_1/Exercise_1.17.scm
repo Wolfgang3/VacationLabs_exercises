6ebd624d7a86ab6b8a679f0aa8f282e943f2bde1
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-05 15:28:31 +0530

    second day

--- exercise_1/Exercise_1.17.scm
+++ exercise_1/Exercise_1.17.scm
@@ -1,12 +1,12 @@
 //multiplication procedure
 =>
-according to the prob new procedures hav to be created like double and half
+according to the prob new procedures hav to be created procedures like double and half
 (define (double num) (+ num num)) 
-(define (halve num) (/ num 2)) 
+(define (half num) (/ num 2)) 
   
 (define (all_opr a n) 
   (cond ((= n 0) 0) 
-        (else (double (* (halve n) a))) 
+        (else (double (* (half n) a))) 
   )
 ) 
 
