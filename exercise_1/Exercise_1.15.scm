a12c5e0939ebd153be76694ac1dc6e61359e60ea
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-05 12:49:09 +0530

    second day

---
+++ exercise_1/Exercise_1.15.scm
@@ -0,0 +1,14 @@
+=> sine of angle
+a) given the value as 12.15
+
+(sine 12.15) 
+(p (sine 4.05))    #else part and divide the angle by 3.0
+(p (p (sine 1.35))) 
+(p (p (p (sine 0.45)))) 
+(p (p (p (p (sine 0.15))))) 
+(p (p (p (p (p (sine 0.05)))))) 
+(p (p (p (p (p 0.05)))))
+...from the bove ste we can see that the sine is no longer in the pitcher therefore sine executes 5 times.
+
+b) the order of growth in space and the number of steps is logarithmic. i.e. in the order of O(log(x)) 
+
