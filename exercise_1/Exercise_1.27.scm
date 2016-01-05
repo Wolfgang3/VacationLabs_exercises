1b0ea0f3950ecf61794c66df4ac91c23286a8588
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-05 18:34:51 +0530

    second day

---
+++ exercise_1/Exercise_1.27.scm
@@ -0,0 +1,22 @@
+// test on given carmichael numbers
+=> 
+// modification was done in fermat-test procedure
+(define (square x) (* x x)) 
+  
+(define (expmod base exp m) 
+  (cond ((= exp 0) 1) 
+        ((even? exp) 
+         (remainder (square (expmod base (/ exp 2) m)) 
+                    m)) 
+        (else 
+         (remainder (* base (expmod base (- exp 1) m)) 
+                    m)))) 
+
+(define (fermat-test_iter? n) 
+  (define (iterate a n) 
+    (if (= a n) true 
+        (if (= (expmod a n n) a) (iterate (+ a 1) n) false))) 
+  (iterate 1 n)) 
+
+
+
