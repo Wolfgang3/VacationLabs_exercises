d2bc0104e608577dae417d38fab27938480ce2f4
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-05 17:58:57 +0530

    second day

---
+++ exercise_1/Exercise_1.26.scm
@@ -0,0 +1,2 @@
+//
+=>because Louis had not use square function, the procedure takes exponential time(as the number increases the time als increases exponentially).Therefore his procedure takes long time.
