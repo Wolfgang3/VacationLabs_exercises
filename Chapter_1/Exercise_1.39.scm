2fc9e8164970cfe28307cc43d377df4fd7aa2975
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-07 16:39:26 +0530

    exercise 1.39 done

--- Chapter_1/Exercise_1.39.scm
+++ Chapter_1/Exercise_1.39.scm
@@ -1,6 +1,30 @@
 // Lamberts formula
 =>
-//
-//
+// using the same procedure of exercise 1.37
+
+(define (cont-frac n d k)
+  (define (iter result count)
+    (if (= count k)
+      result
+    (iter (/ d (+ n result)) (+ count 1))))
+  (iter 1 0)
+)
+
+; as given in the book 
+(define (tan-cf x k)
+  (define (n k)
+    (if 
+      (= k 1)
+      x
+      (- (square x))))
+  (define (d k)
+    (- (* 2 k) 1))
+  (cont-frac-iter n d k)
+)
+
+;first parameter must be i radiants (radiants=deg * pi/ 180)
+ex: (tan-cf (* 30 (/ 3.14 180) ) 10)
+
+
 
 
