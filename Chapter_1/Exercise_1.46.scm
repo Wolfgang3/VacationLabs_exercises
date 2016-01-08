8b75cea3088911ac6cc4427c12985e304f9f4665
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-08 16:01:20 +0530

    46 sqrt incomplete

---
+++ Chapter_1/Exercise_1.46.scm
@@ -0,0 +1,26 @@
+// iterative-improve of sqrt in session 1.1.7
+=> 
+(define tolerance 0.000001)
+
+(define (iterative-improve good-enough? improve)
+  (lambda (guess)
+    (if (good-enough? guess)
+	guess
+	(improve guess)))
+)
+
+(define (good-enough? v1 v2) 
+   (< (/ (abs (- v1 v2)) v2)  tolerance)
+) 
+
+;sqrt with iterative-improve
+(define (sqrt x) 
+
+)
+
+;fixed-point with iterative-improve
+(define (fixed-point f guess) 
+  ((iterative-improve f good-enough?) guess)
+) 
+
+
