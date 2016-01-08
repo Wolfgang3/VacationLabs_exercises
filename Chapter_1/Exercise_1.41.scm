5422a63e5a0997377a6b7b68ce89e3f04900f351
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-08 12:00:11 +0530

    exercise_1.41 complete

---
+++ Chapter_1/Exercise_1.41.scm
@@ -0,0 +1,26 @@
+// double procedure 
+=>
+
+(define (double doub)
+  (lambda (x) (doub (doub x) ))
+)
+
+(define (inc x) (+ x 1) )
+
+; (((double (double double)) inc) 5)
+; value= 21   
+ 
+;(each times a procedure double is called it returns another procedure)
+
+; when we run (((double (double double)) inc) 0)
+; we get value= 16 so we can come to now that 0 is incremented 16 times
+
+
+
+
+
+
+
+
+
+
