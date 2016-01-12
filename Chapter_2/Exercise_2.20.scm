14554ebf250e4f3c2eae550f4fbebd0f11ac7a32
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-12 16:37:32 +0530

    2.20 incomplete

---
+++ Chapter_2/Exercise_2.20.scm
@@ -0,0 +1,16 @@
+// same-parity procedure
+=> 
+;folowing the defination in the exercise
+;
+
+(define (same-parity i . li)
+
+)
+
+;run
+(same-parity 1 2 3 4 5 6 7)
+
+(same-parity 2 3 4 5 6 7)
+
+
+
