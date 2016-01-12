6d49c504ea3a35490e321a6332b372d4f0885657
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-12 17:30:08 +0530

    completed 2.21

--- Chapter_2/Exercise_2.21.scm
+++ Chapter_2/Exercise_2.21.scm
@@ -1,4 +1,14 @@
-// 
+// complete the square-list procedure
 => 
+; with reference map procedure on page no. 105 
 
+(define (square-list items)
+  (if (null? items)
+    nil
+    (cons (square (car items)) (square-list (cdr items)) )
+  )
+)
 
+(define (square-list items)
+  (map square items)
+)
