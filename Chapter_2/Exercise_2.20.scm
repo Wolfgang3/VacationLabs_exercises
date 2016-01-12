1f253bd4f16cad367184e6325935996344304c8f
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-12 16:53:23 +0530

    completed 2.20

--- Chapter_2/Exercise_2.20.scm
+++ Chapter_2/Exercise_2.20.scm
@@ -2,9 +2,18 @@
 => 
 ;folowing the defination in the exercise
 ;
-
+;display the numbers with same parity ,take the 1st number and subrtack it from the next number in the list, if the result is even then append the number to the new list result.
 (define (same-parity i . li)
-
+  (define (iter lst result)
+    (cond ((null? lst) result)
+          ( (even? (- i (car lst)) )
+            (iter (cdr lst) (append result (list (car lst))) )
+          )
+          (else (iter (cdr lst) result))
+          )
+    )
+    (iter li (list i))
+  )
 )
 
 ;run
