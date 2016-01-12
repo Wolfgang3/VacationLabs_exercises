091ad6790cbd91a15cdcb95c4d26d895217f645e
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-12 10:43:59 +0530

    2.15 complete

---
+++ Chapter_2/Exercise_2.15.scm
@@ -0,0 +1,6 @@
+// Eva Lu Ator result
+=>
+; I think eva is correct bcuz as we can see from the earlier procedure par1 has mul-interval, so if there is any error ir may increase cuz of mul-interval. as we can see in par2 it involves div-interval which may cancle out error if it exists.
+
+; so i think par2 is better to compute intervals.
+
