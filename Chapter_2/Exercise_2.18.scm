d6cd556e503892b6d08ca089107ca330d302f014
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-12 13:30:02 +0530

    completed 2.18

--- Chapter_2/Exercise_2.18.scm
+++ Chapter_2/Exercise_2.18.scm
@@ -5,19 +5,18 @@
 (define (append l1 l2)
   (if (null? l1)
       l2
-      (cons (car l1) (append (cdr l1) l2))
+      (cons (car l1) (append (cdr l1) l2)) 
   )
 )
 
 (define (reverse l)
   (if (null? l)
       l
-      (append (cdr l) (car l) )
+      (append (reverse (cdr l)) (list (car l)) ) ; for 1st itteration its calling d procedure reverse with 1st argument as (4 9 16 25)
   )
 )
-
-
 
 ;run
 (reverse (list 1 4 9 16 25))
 ;value= (25 16 9 4 1)
+
