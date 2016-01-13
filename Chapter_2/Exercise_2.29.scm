386d29ef28089d2c99aed5b9ffe3d48725e2b3c0
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-13 15:30:50 +0530

    completed 2.29 half

--- Chapter_2/Exercise_2.29.scm
+++ Chapter_2/Exercise_2.29.scm
@@ -23,5 +23,28 @@
   (car (cdr branch))
 )
 
+;b) procedure for total-weight using 
+(define (total-weight mobile)
+  (cond  ((null? mobile) 0)
+    ((not (pair? mobile)) mobile)
+    (else (+ (total-weight (left-branch mobile))
+	     (total-weight (right-branch mobile))
+          )
+    )
+  )
+)
+
+;c) check for balance tree (not sure)
+(define (balanced mobile)
+  (if (= (* (total-weight (left-branch mobile)) (branch-length (left-branch mobile)))
+         (* (total-weight (right-branch mobile)) (branch-length (right-branch mobile)))
+      )
+  (display "balanced") 
+  (display "not balanced")
+  )
+)
+
+
+
 
 
