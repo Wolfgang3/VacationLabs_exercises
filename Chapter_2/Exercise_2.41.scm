c425815453bd8d6da545b9e6ddeb3628e98c84d9
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-14 18:00:05 +0530

    completed 2.41

---
+++ Chapter_2/Exercise_2.41.scm
@@ -0,0 +1,45 @@
+// 
+=> 
+;referring to previous exercise 2.40
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence))))
+)
+
+(define (flatmap proc seq)
+  (accumulate append () (map proc seq))
+)
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      ()
+      (cons low (enumerate-interval (+ low 1) high))
+  )
+)
+
+(define (unique-pairs n)
+  (flatmap
+        (lambda (i)
+          (map (lambda (j) (list i j))
+              (enumerate-interval 1 (- i 1))))
+        (enumerate-interval 1 n))
+)
+
+;referring to procedure on pg. 123
+(define (unique-triples number)
+  (if (< number 3)
+      ()
+      (append (unique-triples (- number 1))
+         (map (lambda (x) (append x (list number)))
+              (unique-pairs (- number 1))
+         )
+      )
+  )
+)
+
+;run
+(unique-triples 5)
+;value= 
+
