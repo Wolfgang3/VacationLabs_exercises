315829484e52a36f80ee51a5c40b5cabf1b28bc8
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-15 14:40:26 +0530

    completed 2.55

---
+++ Chapter_2/Exercise_2.55.scm
@@ -0,0 +1,6 @@
+// Eva lu Ator
+=> 
+(car ''abracssdd)
+;the above statement prints quote
+; this is because the first quote takes it as a list, so by list the first value of the list is a quote which is returned by car
+
