350710b7dd498c2b9dc1a4e7b6952207b718c298
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-18 19:00:21 +0530

    working on 2.65

--- Chapter_2/Exercise_2.65.scm
+++ Chapter_2/Exercise_2.65.scm
@@ -1,3 +1,18 @@
-// 
+//  
 =>
 
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)
+  )
+)
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (not (element-of-set? (car set1) set2)
+          (adjoin-set (car set1) (union-set (cdr set1) set2)) )
+        (else (union-set (cdr set1) set2))
+  )
+)
