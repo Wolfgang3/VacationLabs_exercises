649b4c9a5e9dea1f94328b34e3a8a630df99e79c
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-20 12:28:21 +0530

    completed 2.72

---
+++ Chapter_2/Exercise_2.72.scm
@@ -0,0 +1,5 @@
+// order of growth of the encoding procedure
+=>
+;refering to frequencies given in 2.71
+;since we hav to search for the entire list, so to encode the most frequent symbol the order of growth will be o(n)
+;for worse case the order of growth will be o(n^2)
