fbd92bca9c80ff8c6d0beba3348af6489750ddd8
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-20 10:49:08 +0530

    completed ex_2.68

--- Chapter_2/Exercise_2.68.scm
+++ Chapter_2/Exercise_2.68.scm
@@ -86,20 +86,11 @@
   )
 )
 
-;from the question
-(define (encode message tree)
-  (if (null? message)
-    '()
-    (append (encode-symbol (car message) tree)
-            (encode (cdr message) tree))
-  )
-)
-
+; referring to exercise 2.59 , to check if the element is in the list
 (define (element-of-set? x set)
   (cond ((null? set) false)
-        ((= x (car set)) true)
-        ((< x (car set)) false)
-        (else (element-of-set? x (cdr set)) )
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))
   )
 )
 
@@ -121,18 +112,45 @@
   (decode-1 bits tree)
 )
 
-;encode-symbol procedure 
-(define (encode-symbol symb tree)
-  (cond ((element-of-set? symb (symbols (left-branch tree)))
-          )
+;encode procedure (from the question) 
+(define (encode message tree)
+  (if (null? message)
+    '()
+    (append (encode-symbol (car message) tree)
+            (encode (cdr message) tree))
+  )
+)
 
+;encode-symbol procedure 
+;first go to all left add keep on adding 0, simillarly while visiting right keep on adding 1
+(define (encode-symbol symb s_tree)
+  (cond ((element-of-set? symb (symbols (left-branch s_tree)))
+         (if (leaf? (left-branch s_tree))
+            '(0); for the left node
+            (cons 0  (encode-symbol symb (left-branch s_tree))) ;else add 0 if it still on the left 
+          )
+        )
+        ((element-of-set? symb (symbols (right-branch s_tree)))
+         (if (leaf? (right-branch s_tree))
+             '(1) ; for the right node
+            (cons 1  (encode-symbol symb (right-branch s_tree))) ;else add 1 if it still on the right 
+          )
+        )
         (else (error "symbol not in the tree"))
   )
 )
 
 ;run 
+; sample-tree drawn on the note book
+
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 (decode sample-message sample-tree)
 ;value= (a d a b b c a)
+(define sample-symbols '(a d a b b c a))
+;(element-of-set? (car sample-symbols) (symbols (left-branch sample-tree)))
+
 ;now the encoding procedure
-(encode '(a d a b b c a) sample-tree)
+(encode sample-symbols sample-tree)
+;value= (0 1 1 0 0 1 0 1 0 1 1 1 0)
+(encode '(x y z) sample-tree)
+;;symbol not in the tree
