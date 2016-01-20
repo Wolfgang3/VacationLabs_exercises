f70c9b338a23b80f8d703cebcc6ba576bd8c1303
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-20 12:09:43 +0530

    completed 2.71

--- Chapter_2/Exercise_2.71.scm
+++ Chapter_2/Exercise_2.71.scm
@@ -1,2 +1,5 @@
 // no. of bits required for the following:
 =>
+;since we hav a huffman tree it sorts the tree in descending order of symbols used
+;so to encode the most frequent symbol it will require only 1 bit
+; and to encode the least frequent symbol it wil take (n-1) bits
