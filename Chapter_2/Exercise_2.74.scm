3181ae4dc9c8b1d4debe677928c63ab926731248
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-20 19:30:24 +0530

    ex_2.74 only d is left

--- Chapter_2/Exercise_2.74.scm
+++ Chapter_2/Exercise_2.74.scm
@@ -3,16 +3,50 @@
 ;part a): procedure for get-record
 
 ;get-record must take two parameters personnel_file and name and return employee details
-;assume that there is a file and in that file all the data of the employees are kept in a row form
+;assume that there is a personnel_file where all the data of the employees are kept in a row form
 (define (get-record personnel_file name)
-  (define (get-record-iter row emp-name personal-file)
+  (define (get-record-iter row personal-file name)
     (if(null? row)
         (display "cant find records of this employee")
       (if (get 'get-record row)
         record
-        (get-record-iter (next-row) emp-name personal-file)
+        (get-record-iter (next-row) personal-file name)
       )
     )
-    (get-record-iter row emp-name personal-file)
+    (get-record-iter row personal-file name)
   )
 )
+
+;part b): procedure for get-salary
+;assume that salary info isplaced in the same record personnel_file 
+(define (get-salary personnel_file name)
+  (define (iter row personal-file name)
+    (if(null? row)
+        (display "cant find record of this employee")
+      (if (get 'get-salary row)
+        (salary record)
+        (iter (next-row) personal-file name)
+      )
+    )
+    (iter row personal-file name)
+  )
+)
+
+;part c): find-employee-record procedure
+;this searches all personnel_files for the record of the employee
+(define (find-employee-record employee_name)
+  (define (personal-file-iter row personal-file name)
+    (define (iter row personal-file name)
+      (if(null? row)
+          (display "cant find record of this employee")
+        (if (get 'find-employee-record row)
+          record
+          (iter (next-row) personal-file name)
+        )
+      )
+      (iter row personal-file name)
+    )
+    (personal-file-iter row personal-file name))
+)
+
+;part d): they must update the info of the employee in the personnel_file
