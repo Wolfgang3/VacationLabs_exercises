cf8895e40c0393355a85a043830cf85308dc76e4
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-25 12:25:40 +0530

    completed 2.92

--- Chapter_2/Exercise_2.92.scm
+++ Chapter_2/Exercise_2.92.scm
@@ -1,6 +1,7 @@
 // extending polynomial package to work with different variables
 =>
 ;with reference from the procedure on pg no. 190
+;using this package we can work with different variables
 (define (install-rational-package)
   ;;internal procedures
   (define (numer x) (car x))
@@ -11,25 +12,24 @@
     ) 
   )
 
+  ;here we change the arithmetic procedures in terms of polynimial
   (define (add-rat x y)
-    (make-rat (+ (* (numer x) (denom y))
-                 (* (numer y) (denom x)))
-              (* (denom x) (denom y)))
+    (make-rat (add-poly (mul-poly (numer x) (denom y))
+                        (mul-poly (numer y) (denom x))) )
   )
 
   (define (sub-rat x y)
-    (make-rat (- (* (numer x) (denom y))
-                 (* (numer y) (denom x))
-              (* (denom x) (denom y))))
+    (make-rat (sub-poly (mul-poly (numer x) (denom y))
+                        (mul-poly  (numer y) (denom x))) )
   )
 
   (define (mul-rat x y)
-    (make-rat (* (numer x) (denom y))
-              (* (numer x) (denom y))))
+    (make-rat (mul-poly (numer x) (denom y))
+              (mul-poly (numer x) (denom y))) )
 
   (define (div-rat x y)
-    (make-rat (* (numer x) (denom y))
-              (* (denom x) (numer y))))
+    (make-rat (mul-poly (numer x) (denom y))
+              (mul-poly (denom x) (numer y))) )
 
   ;;interface to rest of the system
   (define (tag x) (attach-tag 'rational x))
@@ -37,6 +37,12 @@
        (lambda (x y) (tag (add-rat x y))))
   (put 'sub '(rational rational)
        (lambda (x y) (tag (sub-rat x y))))
-  
-  
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done
 )
