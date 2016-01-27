ac7ca0cdfbfa9b86ecf3f10dc98eeb91f440ac5e
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-27 10:34:05 +0530

    2.97 incomplete

---
+++ Chapter_2/Exercise_2.97.scm
@@ -0,0 +1,37 @@
+//
+=>
+
+;part b)
+(define (reduce-integers n d)
+  (let ((g (gcd n d)))
+    (list (/ n g) (/ d g))
+  )
+)
+
+;reduce-terms
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (and (< n 0) (< d 0))
+      (cons (/ (* n -1) g) (/ (* d -1) g))
+      (cons (/ n g) (/ d g))
+    )
+  )
+)
+
+(define (reduce-terms)
+)
+
+
+;write reduce-poly, add-poly
+
+
+;run
+(define p1 (make-polynomial 'x '((1 1) (0 1))))
+(define p2 (make-polynomial 'x '((3 1) (0 -1))))
+(define p3 (make-polynomial 'x '((1 1))))
+(define p4 (make-polynomial 'x '((2 1) (0 -1))))
+
+(define rf1 (make-polynomial p1 p2))
+(define rf2 (make-polynomial p3 p4))
+
+(add rf1 rf2)
