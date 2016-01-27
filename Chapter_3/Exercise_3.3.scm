dcfa2ca11cdbf7963b0ba5f040f2e97e78c1bc6a
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-27 17:51:54 +0530

    completed 3.3

---
+++ Chapter_3/Exercise_3.3.scm
@@ -0,0 +1,34 @@
+// make-account with password
+=>
+;previous procedure of make-account
+(define (make-account balance acc_password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "insufficient funds"
+    )
+  )
+  (define (deposit amount)
+    (set! balance (+ balance amount))  
+  balance)
+
+  (define (dispatch password m)
+    (if (eq? acc_password password)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            (else (error "unknown request -- MAKE-ACCOUNT" m))
+      )
+    ("incorrect password")
+    )
+  )
+dispatch)
+
+
+;run
+(define acc (make-account 100 'aa))
+
+((acc 'ab 'withdraw) 40)
+;Value: 60
+((acc 'bb 'deposit) 50)
+;incorrect password
