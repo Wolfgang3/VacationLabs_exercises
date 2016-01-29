528d2de5bed119fe7fd555763455fe2bcdb0623d
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-01-29 16:11:35 +0530

    completed 3.10

--- Chapter_3/Exercise_3.9.scm
+++ Chapter_3/Exercise_3.9.scm
@@ -1,3 +1,32 @@
-// environment model for factorial procedure
+// alternate syntax for make-withdraw procedure
 =>
-;drawn on the note book
+;using let
+; (let ((<var> <exp>)) <body>)
+(define (make-withdraw initial-amount)
+  (let ((balance initial-amount))
+    (lambda (amount)
+      (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+                balance)
+        "insufficient funds"
+      )
+    )
+  )
+)
+
+;using lambda
+; ((lambda (<var>) <body>) <exp>)
+(define (make-withdraw initial-amount)
+  ((lambda (balance)
+    (lambda (amount)
+      (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+                balance)
+        "insufficient funds"
+      )
+    )
+  )
+  initial-amount)
+)
+
+;=> environment model of the above alternate procedure using lambda is drawn on the note book
