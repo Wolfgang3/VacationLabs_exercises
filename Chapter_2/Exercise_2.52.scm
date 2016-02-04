b132b10a0d21637dd7b45c1320ce9eb1b241f4ca
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-02-04 17:30:23 +0530

    completed 2.52

---
+++ Chapter_2/Exercise_2.52.scm
@@ -0,0 +1,47 @@
+#lang racket
+(require sicp-pict)
+;modify the procedures of previous exercises
+
+;a) change wave
+
+;b) change pattern constructed by corner-split
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+;we want to change the corner-split to copy only 1 up-split and right-split
+;so we can remove beside and below 
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(paint (corner-split einstein 1))
+
+;c) modify square-limit using square-of-four
+;square-of-four
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (square-limit painter n)
+)
+;not done
+
+(paint (square-limit einstein 1))
