aec60fcfadf2cc06c8df544f59b00688ed2ec26e
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-02-04 11:21:19 +0530

    completed 2.51

---
+++ Chapter_2/Exercise_2.51.scm
@@ -0,0 +1,41 @@
+#lang racket
+(require sicp-pict)
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cdr vect))
+;sub-vect
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+;prev transform procedure
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter
+         (make-frame new-origin
+                     (sub-vect (m corner1) new-origin)
+                     (sub-vect (m corner2) new-origin)))))))
+
+;with referece from beside procedure define below painter
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-down
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 1.0 0.0)
+                              split-point))
+          (paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-down frame)
+        (paint-top frame)))))
+
+(paint (below einstein einstein))
+;observed correct output
