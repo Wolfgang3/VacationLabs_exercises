43367d2f7d7d86eec76c03503d9127ed506b1c9a
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-02-09 10:59:30 +0530

    completed 3.19

---
+++ Chapter_3/Exercise_3.19.scm
@@ -0,0 +1,4 @@
+;; modify the prev procedure to take only a constant amount of space 
+;=>
+; i think the previous procedure will take constant amount of space bcuz of iteration.
+; since there is no recursion hence the space will not increase as time increases.
