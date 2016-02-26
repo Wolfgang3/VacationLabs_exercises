64b0c608ad84424b1e03038459b013aa1bc4675a
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-02-26 12:20:03 +0530

    completed 3.32

---
+++ Chapter_3/Exercise_3.32.scm
@@ -0,0 +1,8 @@
+;; y use queue in agenda?
+;=> 
+
+; since all the operations of a procedure are interdependent on each other. it is very import to use queue
+; if we want the correct order of the operation.
+; if use other implementation like stack LIFO we might get wrong output.
+; eg. In half-adder all the next result are dependent on the previous value of the input.
+; taht is why a queue has to used in agenda. 
