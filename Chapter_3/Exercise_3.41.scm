6188c5aab4d514a803c56e4eb3cbee86d46db7a5
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-02-29 16:09:04 +0530

    completed 3.41

--- Chapter_3/Exercise_3.41.scm
+++ Chapter_3/Exercise_3.41.scm
@@ -1,3 +1,3 @@
 ;; 
 ;=> 
-; i dont think there will be any problem cuz bothe the deposit and withdraw wil be safe cuz of the serializer.
+; i dont think there will be any problem cuz both the deposit and withdraw wil be safe cuz of the serializer.
