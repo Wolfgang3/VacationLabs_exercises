85347585f68b9f02907c8df0d4f425d6ce04d627
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-02-29 18:25:37 +0530

    completed 3.44

---
+++ Chapter_3/Exercise_3.44.scm
@@ -0,0 +1,5 @@
+;; 
+;=> 
+; Louis Reasoner is wrong in this case.
+; Transfer doesnt require accounts to be idel.
+; when doing exchange both accounts must not be involve in any other transaction.   
