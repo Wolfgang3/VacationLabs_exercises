d8ca099d5f4d5b828779608efd180bb106819edc
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-03-03 16:38:45 +0530

    completed 3.55 & 3.56

---
+++ Chapter_3/Exercise_3.55.scm
@@ -0,0 +1,9 @@
+;; 
+;=> 
+; 1,1+2,1+2+3,1+2+3+4
+(define (partial-sums s)
+  (cons-stream (stream-car s) 
+               (add-streams (stream-cdr s) 
+                             (partial-sums s))))
+
+;output stream will be 1,3,6,10,15.......
