d42d967094eaad69eb1f86b7532f06875e6c170a
Wolfgang3 <wolfgang_furtado@yahoo.com>
2016-03-03 19:02:19 +0530

    completed 3.57

---
+++ Chapter_3/Exercise_3.57.scm
@@ -0,0 +1,5 @@
+;; 
+;=> 
+; If we implement the fibs procedure without using memo-proc 
+; then the number of steps will increase exponentially.
+; i.e. no. of additions will be equal to the  fibonacci number
